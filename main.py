704071e354d6b73254531934365f1d1e4f559d5e
Wevuni <wevuni@gmail.com>
2022-08-05 10:07:51 -0300

    ee

---
+++ main.py
@@ -0,0 +1,14 @@
+"""create database apostas;
+use apostas;
+create table apostador(
+cpf varchar (17),
+nome varchar (50),
+telefone varchar (30),
+primary key(cpf)
+);
+create table tabelas(
+sequencia int(5),
+cpf1 varchar (17),
+foreign key (cpf) references apostador(cpf));
+
+select * from tabelas"""
