ed798c001a14abd30c5d7002bd18f2180d9b7ca9
Dryzin <endrioemanuel11@gmail.com>
2022-08-05 10:41:35 -0300

    função salvar

--- funcoes.py
+++ funcoes.py
@@ -1,5 +1,7 @@
 import mysql.connector
 from classe_aposta import *
+from classe_participantes import *
+
 class DBAgenda:
     def __init__(self):
         self.conexao = mysql.connector.connect(
@@ -8,3 +10,9 @@ class DBAgenda:
         password='q1w2e3',
         database='apostas')
         self.meu_cursor = self.conexao.cursor()
+
+    def salvar_participante(self, cpf, nome, telefone):
+        objcontato = Info_participantes(cpf, nome, telefone)
+        comando_sql = f'insert into Apostador (cpf, nome, telefone) value ("{objcontato.cpf}", "{objcontato.nome}", "{objcontato.telefone}")'
+        self.meu_cursor.execute(comando_sql)
+        self.conexao.commit()
