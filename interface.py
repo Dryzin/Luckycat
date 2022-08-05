a9927434feb6ef7f88d831d46310f97991acd681
freguesy <freguesy@gmail.com>
2022-08-05 11:14:45 -0300

    1

--- interface.py
+++ interface.py
@@ -3,16 +3,18 @@ from tkinter import *
 #criar janela
 
 root = Tk()
+root.title("密密密密密密密密密密密密密密密密密密密密密密密密密密密密密密密密密密密密密")
 
 bg = PhotoImage(file = "fundo.png") 
-label1 = Label( root, image = bg) 
+label1 = Label( root, image = bg)
 label1.place(x = 0, y = 0) 
 
-root.geometry("700x1000")
+root.geometry("707x1000")
 
 #criar os widgets
+bt = PhotoImage(file = "bt.png") 
 
-bt1 = Button(root, text='Bt1')
+bt1 = Button(root,bg='red' ,image=bt, activebackground="red")
 bt2 = Button(root, text='Bt2')
 bt3 = Button(root, text='Bt3')
 bt4 = Button(root, text='Bt4')
