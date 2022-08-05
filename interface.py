61fc6f0a2e51777426dcfacd2402993d4b26532b
Dryzin <endrioemanuel11@gmail.com>
2022-08-05 10:01:44 -0300

    proporção

--- interface.py
+++ interface.py
@@ -9,21 +9,25 @@ label1 = Label( root, image = bg)
 label1.place(x = 0, y = 0) 
 
 root.geometry("700x1000")
+root.minsize(700, 1000) 
+root.maxsize(700, 1000)
+#geometria
+
 
 #criar os widgets
 
-bt1 = Button(root, text='Bt1')
-bt2 = Button(root, text='Bt2')
-bt3 = Button(root, text='Bt3')
-bt4 = Button(root, text='Bt4')
+bt1 = Button(root, text='Novo Sorteio')
+bt2 = Button(root, text='Ver Sorteio')
+bt3 = Button(root, text='Cadastrar Participantes')
+bt4 = Button(root, text='Finalizar Sorteio')
 
 
 #organizar os widgets
 
-bt1.grid(row=0, column=0, sticky=NSEW)
-bt2.grid(row=0, column=2, sticky=NSEW)
-bt3.grid(row=1, column=0, columnspan=3, sticky=NSEW)
-bt4.grid(row=2, column=0, columnspan=3, sticky=NSEW)
+bt1.place(x= 180, y= 450)
+bt2.place(x= 280, y= 450)
+bt3.place(x= 180, y= 550)
+bt4.place(x= 180, y= 650)
 
 #executar a janela
 root.mainloop()
