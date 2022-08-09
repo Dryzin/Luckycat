79ec7332a988295948c97350fd1bfa855eb21497
Wevuni <wevuni@gmail.com>
2022-08-09 10:28:33 -0300

    commit

--- funcoes.py
+++ funcoes.py
@@ -1,5 +1,6 @@
 import mysql.connector
 from classe_aposta import *
+from classe_participantes import *
 class DBAgenda:
     def __init__(self):
         self.conexao = mysql.connector.connect(
@@ -8,3 +9,12 @@ class DBAgenda:
         password='q1w2e3',
         database='apostas')
         self.meu_cursor = self.conexao.cursor()
+    
+
+    def cadastrar_participante(self, cpf, nome, telefone):
+        obj_apostador = Info_participantes (self, cpf, nome, telefone)
+        comando_sql = f'insert into apostador ' \
+                      f'(cpf, nome, telefone) value ' \
+                      f'("{obj_apostador.cpf}", "{obj_apostador.nome}","{obj_apostador.telefone}")'
+        self.meu_cursor.execute(comando_sql)
+        self.conexao.commit()
