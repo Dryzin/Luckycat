b61a140c8561fcea3382c0cf3592dadb21520301
Wevuni <wevuni@gmail.com>
2022-08-10 10:14:39 -0300

    commit

--- funcoes.py
+++ funcoes.py
@@ -1,6 +1,7 @@
 import mysql.connector
 from classe_aposta import *
 from classe_participantes import *
+import random 
 
 class funcoes:
     def __init__(self):
@@ -10,6 +11,8 @@ class funcoes:
         password='q1w2e3',
         database='apostas')
         self.meu_cursor = self.conexao.cursor()
+    
+    
 
     def cadastrar_participante(self, cpf, nome, telefone):
         obj_apostador = Info_participantes (cpf, nome, telefone)
@@ -19,8 +22,7 @@ class funcoes:
         self.meu_cursor.execute(comando_sql)
         self.conexao.commit()
 
-    def salvar_participante(self, cpf, nome, telefone):
-        objcontato = Info_participantes(cpf, nome, telefone)
-        comando_sql = f'insert into Apostador (cpf, nome, telefone) value ("{objcontato.cpf}", "{objcontato.nome}", "{objcontato.telefone}")'
-        self.meu_cursor.execute(comando_sql)
-        self.conexao.commit()
+    def sortear (self, cpf, sequencia):
+        x = random.sample(range(1,30),5)
+        
+
